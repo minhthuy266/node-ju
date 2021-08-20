36de5c5715517ea292ceb991874971ee6c9629bc
Thuy Nguyen <thuynguyenminh.hus@gmail.com>
2021-08-20 22:26:41 +0700

    Other middleware and data validation

--- Build API/routes/tourRoutes.js
+++ Build API/routes/tourRoutes.js
@@ -7,12 +7,14 @@ const {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const tourRouter = express.Router();
 
 tourRouter.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 tourRouter.route('/tour-stats').get(getTourStats);
+tourRouter.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 // tourRouter.param('id', checkID);
 
