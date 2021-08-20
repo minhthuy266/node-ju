c7c8b74ea33ad7d0a8874004a634f1158451abff
Thuy Nguyen <thuynguyenminh.hus@gmail.com>
2021-08-20 15:36:53 +0700

    Making the API better filtering

--- Build API/controllers/tourController.js
+++ Build API/controllers/tourController.js
@@ -2,7 +2,27 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find({});
+    // BUILD QUERY
+
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    console.log(req.query, queryObj);
+    const query = Tour.find(queryObj);
+
+    // ANOTHER WAY
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
